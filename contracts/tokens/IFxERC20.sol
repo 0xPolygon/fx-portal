2780fdd6b8c6468c7e56c2f215619c92014a412e
Jaynti Kanani <jdkanani@gmail.com>
2021-01-17 16:21:38 +0400

    fix: rename variable

--- contracts/tokens/IFxERC20.sol
+++ contracts/tokens/IFxERC20.sol
@@ -4,7 +4,7 @@ pragma solidity 0.7.3;
 interface IFxERC20 {
     function fxManager() external returns(address);
     function connectedToken() external returns(address);
-    function initialize(address _fxManager,address _rootToken, string memory _name, string memory _symbol, uint8 _decimals) external;
+    function initialize(address _fxManager,address _connectedToken, string memory _name, string memory _symbol, uint8 _decimals) external;
     function mint(address user, uint256 amount) external;
     function burn(address user, uint256 amount) external;
 }
