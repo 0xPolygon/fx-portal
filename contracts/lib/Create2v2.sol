75bc5fac6d86fea670b75b54505d9c84eada2376
QEDK <1994constant@gmail.com>
2021-04-16 20:44:41 +0530

    :new: Add library contracts and upgrade to 0.8.0

---
+++ contracts/lib/Create2v2.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+// Create2 adds common methods for minimal proxy with create2
+abstract contract Create2v2 {
+    // creates clone using minimal proxy
+    function createClone(bytes32 _salt, address _target) internal returns (address _result) {
+        bytes20 _targetBytes = bytes20(_target);
+
+        assembly {
+            let clone := mload(0x40)
+            mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+            mstore(add(clone, 0x14), _targetBytes)
+            mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+            _result := create2(0, clone, 0x37, _salt)
+        }
+
+        require(_result != address(0), "Create2: Failed on minimal deploy");
+    }
+
+    // get computed create2 address
+    function computedCreate2Address(bytes32 salt, bytes32 bytecodeHash, address deployer) internal pure returns (address) {
+        bytes32 _data = keccak256(
+            abi.encodePacked(bytes1(0xff), deployer, salt, bytecodeHash)
+        );
+        return address(uint160(uint256(_data)));
+    }
+
+    // get minimal proxy creation code
+    function minimalProxyCreationCode(address logic) internal pure returns (bytes memory) {
+        bytes10 creation = 0x3d602d80600a3d3981f3;
+        bytes10 prefix = 0x363d3d373d3d3d363d73;
+        bytes20 targetBytes = bytes20(logic);
+        bytes15 suffix = 0x5af43d82803e903d91602b57fd5bf3;
+        return abi.encodePacked(creation, prefix, targetBytes, suffix);
+    }
+}
--- contracts/lib/Merkle2.sol
+++ contracts/lib/Merkle2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.7.3;
+pragma solidity ^0.8.0;
 
-library Merkle {
+library Merkle2 {
     function checkMembership(
         bytes32 leaf,
         uint256 index,
