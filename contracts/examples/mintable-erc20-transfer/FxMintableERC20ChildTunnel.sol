ae5128f20be3aa321220baf5ec8edcd4ce7272b0
qedk <1994constant@gmail.com>
2022-09-29 18:05:42 +0400

    fix: add withdrawTo functionality in mintable erc20 tunnel

--- contracts/examples/mintable-erc20-transfer/FxMintableERC20ChildTunnel.sol
+++ contracts/examples/mintable-erc20-transfer/FxMintableERC20ChildTunnel.sol
@@ -74,6 +74,14 @@ contract FxMintableERC20ChildTunnel is Ownable, FxBaseChildTunnel, Create2 {
     }
 
     function withdraw(address childToken, uint256 amount) public {
+        _withdraw(msg.sender, childToken, amount);
+    }
+
+    function withdrawTo(address receiver, address childToken, uint256 amount) public {
+        _withdraw(receiver, childToken, amount);
+    }
+
+    function _withdraw(address receiver, address childToken, uint256 amount) internal {
         FxERC20 childTokenContract = FxERC20(childToken);
         // child token contract will have root token
         address rootToken = childTokenContract.connectedToken();
@@ -95,7 +103,7 @@ contract FxMintableERC20ChildTunnel is Ownable, FxBaseChildTunnel, Create2 {
         bytes memory metaData = abi.encode(name, symbol, decimals);
 
         // send message to root regarding token burn
-        _sendMessageToRoot(abi.encode(rootToken, childToken, msg.sender, amount, metaData));
+        _sendMessageToRoot(abi.encode(rootToken, childToken, receiver, amount, metaData));
     }
 
     //
