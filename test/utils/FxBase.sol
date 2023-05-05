459b5084067da6e2ea6240d0eeb0746fea4620b3
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2023-05-05 17:08:49 +0530

    chore: mv mocks to test folder

--- test/utils/FxBase.sol
+++ test/utils/FxBase.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "@utils/Test.sol";
+import "@utils/Events.sol";
 import {FxRoot} from "contracts/FxRoot.sol";
 import {FxChild} from "contracts/FxChild.sol";
-import {MockStateSender} from "contracts/mock/StateSender.sol";
-import {MockStateReceiver} from "contracts/mock/StateReceiver.sol";
+import {MockStateSender} from "@mock/StateSender.sol";
+import {MockStateReceiver} from "@mock/StateReceiver.sol";
 
 contract FxBase is Test {
     FxRoot public fxRoot;
