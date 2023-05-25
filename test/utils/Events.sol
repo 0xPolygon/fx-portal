f9bf56d418015ebb74b4b380d03eea726b6a8b6e
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2023-05-26 01:58:49 +0530

    upd: events; fxRoot tests

--- test/utils/Events.sol
+++ test/utils/Events.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 abstract contract Events {
     event TokenMappedERC20(address indexed rootToken, address indexed childToken); // root chain
+    event TokenMappedERC721(address indexed rootToken, address indexed childToken);
     event TokenMapped(address indexed rootToken, address indexed childToken); // child chain
 
     event FxWithdrawERC20(
